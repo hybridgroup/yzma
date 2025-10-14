3d1505e526c1b5ab6e4927ee208a0b35b8f7add4
deadprogram <ron@hybridgroup.com>
2025-10-14 11:41:37 +0200

    refactor: move memory functions into separate file
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- pkg/llama/context.go
+++ pkg/llama/context.go
@@ -46,20 +46,8 @@ var (
 	// LLAMA_API void                           llama_perf_context_reset(      struct llama_context * ctx);
 	perfContextResetFunc ffi.Fun
 
-	// LLAMA_API void llama_memory_clear(
-	// 				llama_memory_t mem,
-	// 				bool data);
-	memoryClearFunc ffi.Fun
-
 	// LLAMA_API llama_memory_t llama_get_memory (const struct llama_context * ctx);
 	getMemoryFunc ffi.Fun
-
-	// LLAMA_API bool llama_memory_seq_rm(
-	//         		llama_memory_t mem,
-	//           	llama_seq_id seq_id,
-	//              llama_pos p0,
-	//              llama_pos p1);
-	memorySeqRmFunc ffi.Fun
 
 	// LLAMA_API void llama_synchronize(struct llama_context * ctx);
 	synchronizeFunc ffi.Fun
@@ -110,16 +98,8 @@ func loadContextFuncs(lib ffi.Lib) error {
 		return loadError("llama_perf_context_reset", err)
 	}
 
-	if memoryClearFunc, err = lib.Prep("llama_memory_clear", &ffi.TypeVoid, &ffi.TypePointer, &ffi.TypeUint8); err != nil {
-		return loadError("llama_memory_clear", err)
-	}
-
 	if getMemoryFunc, err = lib.Prep("llama_get_memory", &ffi.TypePointer, &ffi.TypePointer); err != nil {
 		return loadError("llama_get_memory", err)
-	}
-
-	if memorySeqRmFunc, err = lib.Prep("llama_memory_seq_rm", &ffi.TypeUint8, &ffi.TypePointer, &ffi.TypeSint32, &ffi.TypeSint32, &ffi.TypeSint32); err != nil {
-		return loadError("llama_memory_seq_rm", err)
 	}
 
 	if synchronizeFunc, err = lib.Prep("llama_synchronize", &ffi.TypeVoid, &ffi.TypePointer); err != nil {
@@ -177,24 +157,6 @@ func Decode(ctx Context, batch Batch) int32 {
 // PerfContextReset resets the performance metrics for the model context.
 func PerfContextReset(ctx Context) {
 	perfContextResetFunc.Call(nil, unsafe.Pointer(&ctx))
-}
-
-// MemoryClear clears the memory contents.
-// If data == true, the data buffers will also be cleared together with the metadata.
-func MemoryClear(mem Memory, data bool) {
-	memoryClearFunc.Call(nil, unsafe.Pointer(&mem), unsafe.Pointer(&data))
-}
-
-// MemorySeqRm removes all tokens that belong to the specified sequence and have positions in [p0, p1).
-// Returns false if a partial sequence cannot be removed. Removing a whole sequence never fails.
-// seqID < 0 : match any sequence
-// p0 < 0     : [0,  p1]
-// p1 < 0     : [p0, inf)
-func MemorySeqRm(mem Memory, seqID SeqId, p0, p1 Pos) bool {
-	var result ffi.Arg
-	memorySeqRmFunc.Call(unsafe.Pointer(&result), unsafe.Pointer(&mem), &seqID, &p0, &p1)
-
-	return result.Bool()
 }
 
 // GetMemory returns the current Memory for the Context.
