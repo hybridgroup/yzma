d3f7d87a02de1b5605b9069490218bd05f7fea9c
deadprogram <ron@hybridgroup.com>
2025-10-15 13:39:20 +0200

    ci: avoiding the stack
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- pkg/llama/logs.go
+++ pkg/llama/logs.go
@@ -10,13 +10,14 @@ type LogCallback *ffi.Closure
 
 var (
 	// LLAMA_API void llama_log_set(ggml_log_callback log_callback, void * user_data);
-	logSet     func(cb LogCallback, data uintptr)
 	logSetFunc ffi.Fun
 
 	// static void llama_log_callback_null(ggml_log_level level, const char * text, void * user_data) { (void) level; (void) text; (void) user_data; }
 	logSilent *ffi.Closure
 
-	callback unsafe.Pointer
+	callback    unsafe.Pointer
+	cifCallback ffi.Cif
+	cbFn        uintptr
 )
 
 func loadLogFuncs(lib ffi.Lib) error {
@@ -25,21 +26,17 @@ func loadLogFuncs(lib ffi.Lib) error {
 	if logSetFunc, err = lib.Prep("llama_log_set", &ffi.TypeVoid, &ffi.TypePointer, &ffi.TypePointer); err != nil {
 		return loadError("llama_log_set", err)
 	}
-	logSet = func(cb LogCallback, data uintptr) {
-		logSetFunc.Call(nil, unsafe.Pointer(&cb), unsafe.Pointer(&data))
-	}
 
 	logSilent = ffi.ClosureAlloc(unsafe.Sizeof(ffi.Closure{}), &callback)
 
-	var cifCallback ffi.Cif
 	if status := ffi.PrepCif(&cifCallback, ffi.DefaultAbi, 3, &ffi.TypeVoid, &ffi.TypeSint32, &ffi.TypePointer, &ffi.TypePointer); status != ffi.OK {
 		panic(status)
 	}
 
-	fn := ffi.NewCallback(silentLogCallbackFunc)
+	cbFn = ffi.NewCallback(silentLogCallbackFunc)
 
 	if logSilent != nil {
-		if status := ffi.PrepClosureLoc(logSilent, &cifCallback, fn, nil, callback); status != ffi.OK {
+		if status := ffi.PrepClosureLoc(logSilent, &cifCallback, cbFn, nil, callback); status != ffi.OK {
 			panic(status)
 		}
 	}
@@ -51,6 +48,10 @@ func loadLogFuncs(lib ffi.Lib) error {
 // Note that if you turn logging off when using the [mtmd] package, you must also set Verbosity > 5.
 func LogSet(cb LogCallback, data uintptr) {
 	logSet(cb, data)
+}
+
+func logSet(cb LogCallback, data uintptr) {
+	logSetFunc.Call(nil, unsafe.Pointer(&cb), unsafe.Pointer(&data))
 }
 
 // LogSilent is a callback function that you can pass into the [LogSet] function to turn logging off.
