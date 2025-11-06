fe426ec93cf72e1f04ea38653fa5bb9279d5023b
deadprogram <ron@hybridgroup.com>
2025-11-06 13:31:50 +0100

    feature: add GetModel() function to download package for convenience
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ pkg/download/models_test.go
@@ -0,0 +1,23 @@
+package download
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestGetModel(t *testing.T) {
+	url := "https://huggingface.co/ggml-org/models-moved/resolve/main/tinyllamas/stories260K.gguf"
+	dest := filepath.Join(t.TempDir(), "stories260K.gguf")
+
+	err := GetModel(url, dest)
+	if err != nil {
+		t.Fatalf("GetModel() failed: %v", err)
+	}
+
+	if _, err := os.Stat(dest); os.IsNotExist(err) {
+		t.Fatalf("Downloaded model file not found: %s", dest)
+	}
+
+	t.Logf("GetModel() successfully downloaded the model to: %s", dest)
+}
