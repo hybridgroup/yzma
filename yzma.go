cc10b3c8ec276ab9c0abb0ead775e009e9e36c25
William Kennedy <bill@ardanlabs.com>
2025-11-14 10:17:24 -0500

    fixing linter issues on comment

--- yzma.go
+++ yzma.go
@@ -1,2 +1,3 @@
-// package yzma lets you perform multimodal inference with Vision Language Models (VLMs) by calling llama.cpp without CGo.
+// Package yzma lets you perform multimodal inference with Vision Language
+// Models (VLMs) by calling llama.cpp without CGo.
 package yzma
