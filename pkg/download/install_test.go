2d89b395d8d6e66260090c722cd35ea3e761361b
William Kennedy <bill@ardanlabs.com>
2025-11-15 12:17:01 -0500

    New install libraries function for downloading llamacpp (#78)
    * new install libraries function for downloading llamacpp
    * new version with functions broke down
    * fixing linter issues on comment
    * adding test to install
    * fixing test to not run in GH
    * changes for better readability
    * added a function not needed in yzma
    * renaming test function to match new logic
    * fixing bug with bad path in download get function
    * adding /build/bin back to urls
    * putting // back into files
    * removing //build/bin for window files only
    * adding allow upgrade flag to install
    * removing mkdir call
    * processor support
    * used wrong case for processors
    * error variables should be exported for users
    * added os type support
    * added os check at beginning of install
    * fixing tests by return proper errors
    * adding comment for InstallLibraries function
    * adding CONTRIBUTORS file
    * removing markdown
    * updating the readme file

---
+++ pkg/download/install_test.go
@@ -0,0 +1,58 @@
+package download
+
+import (
+	"runtime"
+	"testing"
+)
+
+func TestInstall(t *testing.T) {
+	if runtime.GOOS == "darwin" {
+		t.Skip("skipping test since github API sends 403 error")
+	}
+
+	dest := t.TempDir()
+
+	exists := alreadyInstalled(dest)
+	if exists {
+		t.Fatalf("should NOT see libraries are installed")
+	}
+
+	if err := initialInstall(dest, CPU); err != nil {
+		t.Fatalf("should be able to install libraries: %v", err)
+	}
+
+	exists = alreadyInstalled(dest)
+	if !exists {
+		t.Fatalf("should see libraries are installed")
+	}
+
+	if err := createVersionFile(dest, "1.0.0"); err != nil {
+		t.Fatalf("should be able update the version file is a different version: %v", err)
+	}
+
+	isLatest, version1, err := alreadyLatestVersion(dest)
+	if err != nil {
+		t.Fatalf("should be able to check if the version is latest: %v", err)
+	}
+
+	if isLatest {
+		t.Fatalf("should NOT see that the version is latest")
+	}
+
+	if err := upgradeInstall(dest, CPU, version1); err != nil {
+		t.Fatalf("should be able to upgrade the libraries: %v", err)
+	}
+
+	isLatest, version2, err := alreadyLatestVersion(dest)
+	if err != nil {
+		t.Fatalf("should be able to check if the version is latest: %v", err)
+	}
+
+	if !isLatest {
+		t.Fatalf("should see that the version is latest")
+	}
+
+	if version1 != version2 {
+		t.Fatalf("should see that the version is updated")
+	}
+}
