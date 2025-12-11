131357d8c520ed76216cf3cdde6af677fd44f537
deadprogram <ron@hybridgroup.com>
2025-12-11 10:56:10 +0100

    docs: some minor docs clarification
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- yzma.go
+++ yzma.go
@@ -1,3 +1,12 @@
-// Package yzma lets you perform multimodal inference with Vision Language
-// Models (VLMs) by calling llama.cpp without CGo.
+// yzma lets you write Go applications that directly integrate llama.cpp
+// for fully local inference using hardware acceleration.
+//
+// Run the latest Vision Language Models and Large Language Models
+// on Linux, on macOS, and on Windows.
+// Use hardware acceleration such as CUDA, Metal, and Vulkan.
+//
+// yzma uses the purego (https://github.com/ebitengine/purego) and
+// ffi (https://github.com/JupiterRider/ffi) packages so CGo is not needed.
+//
+// This also means that yzma works with the newest llama.cpp releases.
 package yzma
