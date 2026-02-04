b18c02df7ee3b1314e531c9ae7d0e42f07ce0ff0
deadprogram <ron@hybridgroup.com>
2026-02-04 12:39:26 +0100

    feature: add Batch helper functions for #165
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- pkg/llama/batch.go
+++ pkg/llama/batch.go
@@ -82,3 +82,44 @@ func BatchGetOne(tokens []Token) Batch {
 
 	return batch
 }
+
+// Clear resets the token count of the batch to zero.
+func (b *Batch) Clear() error {
+	b.NTokens = 0
+
+	return nil
+}
+
+// SetLogit sets whether to compute logits for the token at index idx in the batch.
+func (b *Batch) SetLogit(idx int32, logits bool) {
+	logitPtr := &unsafe.Slice((*int8)(b.Logits), int(b.NTokens))[idx]
+	if logits {
+		*logitPtr = 1
+	} else {
+		*logitPtr = 0
+	}
+}
+
+// Add adds a token to the batch with the given position, sequence IDs, and logits flag.
+func (b *Batch) Add(token Token, pos Pos, seqIDs []SeqId, logits bool) {
+	i := b.NTokens
+
+	// Set token and position
+	unsafe.Slice((*Token)(b.Token), int(b.NTokens+1))[i] = token
+	unsafe.Slice((*Pos)(b.Pos), int(b.NTokens+1))[i] = pos
+
+	// Set number of sequence IDs
+	unsafe.Slice((*int32)(b.NSeqId), int(b.NTokens+1))[i] = int32(len(seqIDs))
+
+	// Set sequence IDs if present
+	seqIDPtrs := unsafe.Slice((**SeqId)(b.SeqId), int(b.NTokens+1))
+	if seqIDPtrs[i] != nil && len(seqIDs) > 0 {
+		seqSlice := unsafe.Slice((*SeqId)(seqIDPtrs[i]), len(seqIDs))
+		for j, sid := range seqIDs {
+			seqSlice[j] = sid
+		}
+	}
+
+	b.NTokens++
+	b.SetLogit(i, logits)
+}
