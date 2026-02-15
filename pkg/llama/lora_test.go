ac20b642620b6bc12423333de965d06a0423e912
deadprogram <ron@hybridgroup.com>
2026-02-15 08:39:30 +0100

    updates: breaking changes to llama.cpp API for LoRa
    This changes the yzma API to match breaking upstream changes in
    llama.cpp API for LoRa models.
    
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- pkg/llama/lora_test.go
+++ pkg/llama/lora_test.go
@@ -150,7 +150,7 @@ func TestAdapterMetaValStr(t *testing.T) {
 	t.Logf("AdapterMetaValStr returned: %s", val)
 }
 
-func TestSetAdapterLora(t *testing.T) {
+func TestSetAdaptersLora(t *testing.T) {
 	modelFile := testLoraModelFileName(t)
 	loraFile := testLoraAdaptorFileName(t)
 
@@ -175,74 +175,17 @@ func TestSetAdapterLora(t *testing.T) {
 	}
 	defer AdapterLoraFree(adapter)
 
-	ret := SetAdapterLora(ctx, adapter, 1.0)
+	adapter2, err := AdapterLoraInit(model, loraFile)
+	if err != nil {
+		t.Fatalf("AdapterLoraInit failed: %v", err)
+	}
+	defer AdapterLoraFree(adapter2)
+
+	ret := SetAdaptersLora(ctx, []AdapterLora{adapter, adapter2}, []float32{1.0, 1.0})
 	if ret != 0 {
-		t.Fatalf("SetAdapterLora failed, return code: %d", ret)
+		t.Fatalf("SetAdaptersLora failed, return code: %d", ret)
 	}
-	t.Logf("SetAdapterLora succeeded")
-}
-
-func TestRmAdapterLora(t *testing.T) {
-	modelFile := testLoraModelFileName(t)
-	loraFile := testLoraAdaptorFileName(t)
-
-	testSetup(t)
-	defer testCleanup(t)
-
-	model, err := ModelLoadFromFile(modelFile, ModelDefaultParams())
-	if err != nil {
-		t.Fatalf("ModelLoadFromFile failed: %v", err)
-	}
-	defer ModelFree(model)
-
-	ctx, err := InitFromModel(model, ContextDefaultParams())
-	if err != nil {
-		t.Fatalf("InitFromModel failed: %v", err)
-	}
-	defer Free(ctx)
-
-	adapter, err := AdapterLoraInit(model, loraFile)
-	if err != nil {
-		t.Fatalf("AdapterLoraInit failed: %v", err)
-	}
-	defer AdapterLoraFree(adapter)
-
-	SetAdapterLora(ctx, adapter, 1.0)
-	ret := RmAdapterLora(ctx, adapter)
-	if ret != 0 {
-		t.Fatalf("RmAdapterLora failed, return code: %d", ret)
-	}
-	t.Logf("RmAdapterLora succeeded")
-}
-
-func TestClearAdapterLora(t *testing.T) {
-	modelFile := testLoraModelFileName(t)
-	loraFile := testLoraAdaptorFileName(t)
-
-	testSetup(t)
-	defer testCleanup(t)
-
-	model, err := ModelLoadFromFile(modelFile, ModelDefaultParams())
-	if err != nil {
-		t.Fatalf("ModelLoadFromFile failed: %v", err)
-	}
-	defer ModelFree(model)
-
-	ctx, err := InitFromModel(model, ContextDefaultParams())
-	if err != nil {
-		t.Fatalf("InitFromModel failed: %v", err)
-	}
-	defer Free(ctx)
-
-	adapter, err := AdapterLoraInit(model, loraFile)
-	if err != nil {
-		t.Fatalf("AdapterLoraInit failed: %v", err)
-	}
-	defer AdapterLoraFree(adapter)
-
-	SetAdapterLora(ctx, adapter, 1.0)
-	ClearAdapterLora(ctx)
-	t.Logf("ClearAdapterLora succeeded")
+	t.Logf("SetAdaptersLora succeeded")
 }
 
 func TestAdapterGetAloraNInvocationTokens(t *testing.T) {
