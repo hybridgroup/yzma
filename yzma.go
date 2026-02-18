c5d189a5fd1accb990ba7794df2eacc6df249b40
deadprogram <ron@hybridgroup.com>
2026-02-18 14:50:54 +0100

    docs: some improvements to the MODELS docs
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- yzma.go
+++ yzma.go
@@ -1,12 +1,9 @@
-// yzma lets you write Go applications that directly integrate llama.cpp
+// yzma lets you write Go applications that directly integrate llama.cpp (https://github.com/ggml-org/llama.cpp)
 // for fully local inference using hardware acceleration.
 //
-// Run the latest Vision Language Models and Large Language Models
-// on Linux, on macOS, and on Windows.
-// Use hardware acceleration such as CUDA, Metal, and Vulkan.
-//
-// yzma uses the purego (https://github.com/ebitengine/purego) and
-// ffi (https://github.com/JupiterRider/ffi) packages so CGo is not needed.
-//
-// This also means that yzma works with the newest llama.cpp releases.
+//   - Run the latest Vision Language Models (VLM) and Large/Small/Tiny Language Models (LLM) on Linux, macOS, or Windows.
+//   - Use any available hardware acceleration such as CUDA (https://en.wikipedia.org/wiki/CUDA), Metal (https://en.wikipedia.org/wiki/Metal_(API)),
+//     or Vulkan (https://en.wikipedia.org/wiki/Vulkan) for maximum performance.
+//   - yzma uses the purego (https://github.com/ebitengine/purego) and  ffi (https://github.com/JupiterRider/ffi) packages so CGo is not needed.
+//   - Works with the newest llama.cpp releases so you can use the latest features and model support.
 package yzma
