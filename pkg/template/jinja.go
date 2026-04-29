29768e358b8a0502db0a4e3e02df9d9fd9c685ea
deadprogram <ron@hybridgroup.com>
2026-04-29 09:36:14 +0200

    chore: switch template engine to ardanlabs/jinja
    This replaces the `github.com/nikolalohinski/gonja/v2` package
    with `github.com/ardanlabs/jinja` for applying Jinja chat templates.
    
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- pkg/template/jinja.go
+++ pkg/template/jinja.go
@@ -1,59 +1,31 @@
 package template
 
 import (
-	"errors"
-	"io"
-
+	"github.com/ardanlabs/jinja"
 	"github.com/hybridgroup/yzma/pkg/message"
-	"github.com/nikolalohinski/gonja/v2"
-	"github.com/nikolalohinski/gonja/v2/exec"
-	"github.com/nikolalohinski/gonja/v2/loaders"
 )
 
 // Apply applies a jinja chat template to a slice of [message.Message], Set addAssistantPrompt to true to generate the
 // assistant prompt, for example on the first message.
 func Apply(tmpl string, messages []message.Message, addAssistantPrompt bool) (string, error) {
-	// prevent filesystem access
-	gonja.DefaultLoader = &NoFSLoader{}
-	// disable logging
-	gonja.SetLoggerOutput(io.Discard)
-
-	t, err := gonja.FromString(tmpl)
+	t, err := jinja.Compile(tmpl)
 	if err != nil {
 		return "", err
 	}
 
-	msgs := make([]map[string]interface{}, len(messages))
+	msgs := make([]any, len(messages))
 	for i, m := range messages {
-		msgs[i] = map[string]interface{}{
+		msg := map[string]any{
 			"role": m.GetRole(),
 		}
 		for k, v := range m.GetContent() {
-			msgs[i][k] = v
+			msg[k] = v
 		}
+		msgs[i] = msg
 	}
 
-	data := exec.NewContext(map[string]interface{}{
+	return t.Render(map[string]any{
 		"messages":              msgs,
 		"add_generation_prompt": addAssistantPrompt,
 	})
-
-	return t.ExecuteToString(data)
-}
-
-// NoFSLoader is a template loader that provides no filesystem access.
-// This prevents template injection attacks like {% include "/etc/passwd" %}.
-type NoFSLoader struct{}
-
-func (nl *NoFSLoader) Read(path string) (io.Reader, error) {
-	return nil, errors.New("filesystem access disabled")
-}
-
-// Resolve always returns an error to prevent filesystem access.
-func (nl *NoFSLoader) Resolve(path string) (string, error) {
-	return "", errors.New("filesystem access disabled")
-}
-
-func (nl *NoFSLoader) Inherit(from string) (loaders.Loader, error) {
-	return nil, errors.New("filesystem access disabled")
 }
